abe403c2f746f154a20fe5acda9cc96e0946f915
Dongsik0000 <panda1842@naver.com>
2025-01-14 10:29:22 +0900

    feat: 문의하기 api 테스트용 작성

--- app.js
+++ app.js
@@ -2,25 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  console.log('Got a GET request from Client')
-  res.send('Got a response from Server')
+app.post('/api/contact', (req, res) => {
+  const name = req.body.name;
+  const phone = req.body.name;
+  const email = req.body.name;
+  const memo = req.body.name;
+
+  const data = `${name} ${phone} ${email} ${memo}`
+
+  res.send(data)
 })
 
-app.post('/', (req, res) => {
-  console.log('Got a POST request from Client')
-  res.send('Got a response from Server')
-})
-
-app.put('/user', (req, res) => {
-  console.log('Got a PUT request from Client')
-  res.send('Got a response from Server')
-})
-
-app.delete('/user', (req, res) => {
-  console.log('Got a DELETE request from Client')
-  res.send('Got a response from Server')
-})
 
 app.listen(port, () => {
   console.log(`Node Legacy App listening on port ${port}`)
