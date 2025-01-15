04f5b2674c2a5e24f890932b2869473dfe156686
Dongsik0000 <panda1842@naver.com>
2025-01-15 16:41:11 +0900

    문의사항 삭제 기능 추가

--- app.js
+++ app.js
@@ -89,6 +89,20 @@ app.get('/contactList', (req, res) => {
   });
 });
 
+app.post('/api/contactDelete/:id', (req, res) => {
+  const id = rep.params.id;
+  const deleteQuery = `DELETE FROM contact WHERE id ='${id}'`;
+  connectionPool.query(deleteQuery, (err,result) =>{
+    if (err) {
+      console.error('데이터 삭제 중 에러 발생', err);
+      res.status(500).send('내부 서버 오류');
+    } else {
+      console.log('데이터가 삭제되었습니다.')
+      res.send("<script>alert('문의사항이 삭제되었습니다.'); location.href='/contactList'</script>")
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Node Legacy App listening on port ${port}`)
 })
