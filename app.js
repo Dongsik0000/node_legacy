57a7af8ec43d0fe9267e3e7774667a2892f2749f
Dongsik0000 <panda1842@naver.com>
2025-01-15 16:23:55 +0900

    style : 목록 테이블 구분선 추가

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ app.post('/api/contact', (req, res) => {
 });  
 
 app.get('/contactList', (req, res) => {
-  const selectQuery = `SELECT * FROM contact ORDER BY ID DESC`
+  const selectQuery = `SELECT * FROM contact ORDER BY ID DESC;`
 
-  connectionPool.query(SQL_Query, (err, result) => {  
+  connectionPool.query(selectQuery, (err, result) => {  
     if (err) {  
       console.error('데이터 삽입 중 에러 발생: ', err);  
       res.status(500).send('내부 서버 오류');  
