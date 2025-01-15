6bf947ef2a36b527a4cc75b5772d22c965c7eee2
Dongsik0000 <panda1842@naver.com>
2025-01-15 15:27:24 +0900

    feat: 문의사항 작성 기능 추가(MySQL 커넥션 풀로 변경)

--- app.js
+++ app.js
@@ -15,15 +15,27 @@ app.use(bodyParser.urlencoded({extended:false}))
 // parsing JSON
 app.use(bodyParser.json())
 
-// MySQL Connection
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  insecureAuth: true,
-});
+// MySQL Connection Pool :
+// MySQL 커넥션을 사용할 때는, 주로 커넥션 풀을 이용하여 관리하는 것이 권장된다.
+const connectionPool = mysql.createPool({  
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER,  
+  password: process.env.DB_PW,  
+  port: process.env.DB_PORT,  
+  database: process.env.DB_NAME,  
+  connectionLimit: 10,  
+  insecureAuth: true,  
+});  
+
+// MySQL connection check
+connectionPool.getConnection((err, connection) => {  
+  if (err) {  
+    console.error('MySQL 연결 중 에러 발생: ', err);  
+  } else {  
+    console.log('MySQL에 연결되었습니다.');  
+    connection.release(); // 커넥션 반환  
+  }  
+});  
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -41,18 +53,26 @@ app.get('/contact', (req, res) => {
   res.render('contact')
 })
 
-app.post('/api/contact', (req, res) => {
-  const name = req.body.name;
-  const phone = req.body.phone;
-  const email = req.body.email;
-  const memo = req.body.memo;
+app.post('/api/contact', (req, res) => {  
+  const name = req.body.name;  
+  const phone = req.body.phone;  
+  const email = req.body.email;  
+  const memo = req.body.memo;  
 
-  const data = `${name} ${phone} ${email} ${memo}`
-
-  res.send(data)
-})
-
-
+  // Ensure values are correctly formatted  
+  const SQL_Query = `INSERT INTO contact(name, phone, email, memo, create_at, modify_at)  
+   VALUES ('${name}', '${phone}', '${email}', '${memo}', NOW(), NOW())`;  
+   
+  connectionPool.query(SQL_Query, (err, result) => {  
+    if (err) {  
+      console.error('데이터 삽입 중 에러 발생: ', err);  
+      res.status(500).send('내부 서버 오류');  
+    } else {  
+      console.log('데이터가 삽입 되었습니다.');  
+      res.send("<script>alert('문의사항이 등록되었습니다.'); location.href='/' </script>" );  
+    }  
+  });  
+});  
 
 app.listen(port, () => {
   console.log(`Node Legacy App listening on port ${port}`)
